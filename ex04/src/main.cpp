7840018a4d6b80167b337d8c365cffa0a994c6c8
iuryr <iuryroberto@gmail.com>
2024-07-24 15:09:33 -0300

    ex04: relocating logic to a function

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -3,40 +3,8 @@
 #include <sstream>
 #include <sys/stat.h>
 
-int main(int argc, char **argv)
+static int replace(std::string file_path, std::string s1, std::string s2)
 {
-	if (argc != 4)
-	{
-		std::cout << "Usage: ./replace filepath s1 s2" << std::endl;
-		std::cout << "Replaces all filepath's ocurrences of s1 by s2 and outputs to filepath.replace file" << std::endl;
-		return 1;
-	}
-
-	struct stat pathstat;
-	if (stat(argv[1], &pathstat) ==	0)
-	{
-		if (S_ISDIR(pathstat.st_mode) != 0)
-		{
-			std::cout << "File is a directory. Quitting." << std::endl;
-			return 1;
-		}
-	}
-	else
-	{
-		std::cout << "Problem verifying input file. Quitting." << std::endl;
-		return 1;
-	}
-
-	std::string	file_path = argv[1];
-	std::string	s1 = argv[2];
-	std::string s2 = argv[3];
-
-	if (s1.empty() == true)
-	{
-		std::cout << "Cannot substitute emptyness by anything. Quitting." << std::endl;
-		return 1;
-	}
-
 	std::ifstream	input_stream(file_path.c_str()); //trying to open file into input file stream
 	if (input_stream.is_open() == true)
 	{
@@ -77,6 +45,46 @@ int main(int argc, char **argv)
 	}
 	output_file_stream << output_string;
 	output_file_stream.close();
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "Usage: ./replace filepath s1 s2" << std::endl;
+		std::cout << "Replaces all filepath's ocurrences of s1 by s2 and outputs to filepath.replace file" << std::endl;
+		return 1;
+	}
+
+	struct stat pathstat;
+	if (stat(argv[1], &pathstat) ==	0)
+	{
+		if (S_ISDIR(pathstat.st_mode) != 0)
+		{
+			std::cout << "File is a directory. Quitting." << std::endl;
+			return 1;
+		}
+	}
+	else
+	{
+		std::cout << "Problem verifying input file. Quitting." << std::endl;
+		return 1;
+	}
+
+	std::string	file_path = argv[1];
+	std::string	s1 = argv[2];
+	std::string s2 = argv[3];
+
+	if (s1.empty() == true)
+	{
+		std::cout << "Cannot substitute emptyness by anything. Quitting." << std::endl;
+		return 1;
+	}
+
+	if (replace(file_path, s1, s2) == 1)
+		return 1;
 
 	return 0;
 }
